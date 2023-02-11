a2b45015c37d47f0077b48eeb2c0cd3ed6b147c1
Bhaargavi N <bhaargavi.n@tringapps.com>
2023-02-11 16:08:49 +0530

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,38 @@
+package org.example;
+import java.io.File;
+import java.util.HashMap;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Main {
+        public static void main(String[] args) throws Exception {
+            Logger l = Logger.getLogger("kitty");
+            String f = "C:\\frquency\\textfile.txt";
+            Scanner sc = new Scanner(new File(f));
+            String data = sc.nextLine();
+            String[] arr = data.split(" ");
+            HashMap<String, Integer> hMap = new HashMap<>();
+            for (int i = 0; i < arr.length; i++) {
+                if (hMap.containsKey(arr[i])) {
+                    int count = hMap.get(arr[i]);
+                    hMap.put(arr[i], count + 1);
+                } else {
+                    hMap.put(arr[i], 1);
+                }
+            }
+            l.log(Level.INFO,() ->" "+hMap);
+            PriorityQueue<HashMap.Entry<String, Integer>> queue = new PriorityQueue<>((a, b) -> {
+                return b.getValue() - a.getValue();
+            });
+            {
+                for (HashMap.Entry<String, Integer> e : hMap.entrySet()) {
+                    queue.add(e);
+                }
+                while (!queue.isEmpty()) {
+                    l.log(Level.INFO,() ->"\n" + queue.poll());
+                }
+            }
+        }
+    }
